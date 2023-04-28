953808b779852e56992ccedba64d5c3ade9ee270
Garalez <vladgalcev03@gmail.com>
2023-04-28 15:29:53 +0300

    wip

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
+
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
+app.use(cors()); // add the cors middleware
 
 let userData = null;
 
